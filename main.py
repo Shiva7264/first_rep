a36c121247d58d3361bc0c8f17af99725f28cc2f
Shiva7264 <104266425+Shiva7264@users.noreply.github.com>
2024-01-16 16:24:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,5 +37,4 @@ if predicted_class[0]==0:
     print('person will die')
 else:
     print('The Person will Survive.')
-
-    st.write(f"Predicted class: {predicted_class[0]}")
+st.write(f"Predicted class: {predicted_class[0]}")
